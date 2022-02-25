4659d83cf37222737b3e22b51743cdc2ed220714
André Luis <anddreluis98@gmail.com>
2022-02-24 23:44:18 -0300

    home criada

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,43 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { BrowserRouter as Router, Link, Route } from "react-router-dom"
 import './App.scss'
+import { api } from './services/api'
 
 function App() {
- 
+
+  const [char, setChar] = useState([])
+
+  useEffect(async () => {
+    const response = await api.get("character")
+
+    setChar(response.data.results.slice(0, 5))
+  }, []
+  )
+
   return (
-  <h1>App Rick and Morty</h1>
+    <>
+
+      <div className='header'>
+        <img src="../assets/img/Rick_and_Morty_logo.svg" />
+      </div>
+      <Router>
+        <div className='cards'>
+
+
+          {char.map(resp => {
+            return (
+              <div className='card'>
+                <img src={resp.image} className='avatar' key={resp.id} />
+                <div className='card-name'>
+                  {resp.name }
+                </div>
+              </div>
+            )
+          })}
+        </div>
+
+      </Router>
+    </>
   )
 }
 
